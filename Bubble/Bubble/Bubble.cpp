02cb5b96e50d84dd09c617b524a92b9dfc1a3411
MrAzarel <93323890+MrAzarel@users.noreply.github.com>
2022-03-03 11:12:16 +0300

    Add files via upload

---
+++ Bubble/Bubble/Bubble.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int* mass; 
+    int size; 
+
+    cout << "n = ";
+    cin >> size;
+
+    if (size <= 0) {
+        cerr << "Invalid size" << endl;
+        return 1;
+    }
+
+    mass = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        cout << "arr[" << i << "] = ";
+        cin >> mass[i];
+    }
+
+    int t;
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (mass[j] > mass[j + 1]) {
+                t = mass[j];
+                mass[j] = mass[j + 1];
+                mass[j + 1] = t;
+            }
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << mass[i] << " ";
+    }
+    cout << endl;
+
+    delete[] mass; 
+
+    return 0;
+}
+
